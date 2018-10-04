86a4b17df3cfc10678b1d5bd806efdc337b30734
Scarlet McLearn <ratul_shams@yahoo.com>
2018-10-04 03:19:32 +0000

    sdfsd

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 /* JavaScript code here */
 
 console.log("External");
+
+function sumArray(arr)
+{
+    var sum = 0;
+    
+    arr.forEach(function(value, index)
+    {
+        sum = sum + value;
+    });
+    
+    return sum;
+}
+
+console.log(sumArray([1,2,3,4,5,6]));
