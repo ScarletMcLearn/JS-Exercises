05cb4e469e6d1d0d98ea02c4166973aa89ae284d
Scarlet McLearn <ratul_shams@yahoo.com>
2018-10-08 03:12:25 +0000

    tasng

--- script.js
+++ script.js
@@ -67,3 +67,40 @@ function getReadingStatus(index)
 
 
 console.log(getReadingStatus(2));
+
+
+
+
+var cart = [ 
+   {
+       name: 'Shoes',
+       price: 560,
+       quantity: 4
+   },
+   {
+       name: 'Regular Tees',
+       price: 455.50,
+       quantity: 6
+   },
+   {
+       name: 'Socks',
+       price: 65.99,
+       quantity: 2
+   }];
+
+
+
+function addItem(newItem)
+{
+    cart.push(newItem);
+    
+    console.log(cart);
+}
+
+var newItem = {
+    name : 'EzLyf',
+    price : 666,
+    quantity : 1000
+};
+
+addItem(newItem);
